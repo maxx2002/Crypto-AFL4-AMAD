862d7ffc249c6ed647ed74f263d7ef34eb2eb277
Maximus Aurelius Wiranata <83909501+maxx2002@users.noreply.github.com>
2022-05-24 08:24:29 +0700

    Initial commit

---
+++ AFL4/AFL4App.swift
@@ -0,0 +1,17 @@
+//
+//  AFL4App.swift
+//  AFL4
+//
+//  Created by Macbook Pro on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AFL4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AFL4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AFL4
+//
+//  Created by Macbook Pro on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
